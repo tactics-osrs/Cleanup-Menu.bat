4de5d1cb38a085e12f2ef280262a9725bbb3619e
Tactics <76490725+tactics-osrs@users.noreply.github.com>
2024-02-27 14:25:09 -0500

    pdated Version

--- Cleanup Menu.bat
+++ Cleanup Menu.bat
@@ -4,6 +4,7 @@ REM Written By: Tactics
 
 @echo off
 color 0A
+
 :Start
 cls
 echo.
@@ -12,8 +13,8 @@ echo 1. Open recycle bin
 echo 2. Delete temporary files 
 echo 3. Deployment Image Servicing and Management tool
 echo 4. System File Checker
-echo 5. Disk clean up
-echo 6. Disk Cleanup Settings
+echo 5. Set and Run Disk Clean up
+echo 6. Help (Functions Descriptions)
 echo 7. Exit
 echo.
 
@@ -21,9 +22,9 @@ REM Code to take user input to choose what command to execute...
 set "choice="
 set /p choice=Enter your choice: 
 
-REM Check if the input is a number between 1 and 8
-echo.%choice%|findstr /r /c:"^[1-8]$" >nul || (
-    echo Invalid input! Please enter a number between 1 and 8.
+REM Check if the input is a number between 1 and 7
+echo.%choice%|findstr /r /c:"^[1-7]$" >nul || (
+    echo Invalid input! Please enter a number between 1 and 7.
     pause
     goto Start
 )
@@ -49,8 +50,6 @@ if "%choice%"=="2" (
     goto Start
 )
 
-
-
 REM Code to run DISM tool...
 if "%choice%"=="3" (
 	echo Beginning system scan. This process will take some time.
@@ -68,8 +67,10 @@ if "%choice%"=="4" (
 	goto Start
 )
 
-REM Run Cleanmgr Disk Clean up...
+REM Set/Run Cleanmgr Disk Clean up...
 if "%choice%"=="5" (
+    echo Setting up disk clean up...
+    cleanmgr /sageset:1
     echo Running disk clean up...
     cleanmgr /sagerun:1
     echo Disk clean up completed.
@@ -77,12 +78,24 @@ if "%choice%"=="5" (
     goto Start
 )
 
-REM Open disk cleanup settings...
 if "%choice%"=="6" (
-	echo Opening Disk Cleanup settings...
-	cleanmgr /sageset:1
-	pause
-	goto Start
+    echo Here are the functions:
+    echo 1. Open recycle bin: Opens the recycle bin via directory.
+	echo.
+    echo 2. Delete temporary files: Removes temp files, directories, and subdirectories, then creates clean directory files them.
+	echo.    
+	echo 3. Deployment Image Servicing and Management tool: Begins system scan to check for corruption and automatically fix any problems it finds as well as clean up the system image on the running operating system.
+	echo.    
+	echo 4. System File Checker: Runs Command-Line Windows tool SFC to varify the integrity of every protected operating system file on your computer. If the utility finds any files that have wrong versions, it replaces them with the correct, varified ones.
+	echo.    
+	echo 5. Disk clean up: Runs Disk Cleanup tool settings first for user to set preferences, then performs a Disk Cleanup.
+	echo.    
+	echo 6. Help: Displays this help message.
+	echo.    
+	echo 7. Exit: Exits the program.
+	echo.
+    pause
+    goto Start
 )
 
 REM Code for exiting...
@@ -90,8 +103,5 @@ if "%choice%"=="7" (
     echo Exiting...
     exit /b
 )
-
-goto Start
-
 
 goto Start
